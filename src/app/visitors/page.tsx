162918943f7952d078a0cc3cc96fa0cb29f98634
George makulu - Amosclaud <georgekbbito@gmail.com>
2026-06-12 10:00:55 -0500

    Update page.tsx

--- src/app/visitors/page.tsx
+++ src/app/visitors/page.tsx
@@ -1,1 +1,9 @@
 
+https://amosclaud-clean-git-main-amosclaud-s-projects.vercel.app/visitors
+https://amosclaud-clean-git-main-amosclaud-s-projects.vercel.app/general-users
+https://amosclaud-clean-git-main-amosclaud-s-projects.vercel.app/creator
+https://amosclaud-clean-git-main-amosclaud-s-projects.vercel.app/feed
+https://amosclaud-clean-git-main-amosclaud-s-projects.vercel.app/explore
+https://amosclaud-clean-git-main-amosclaud-s-projects.vercel.app/marketplace
+https://amosclaud-clean-git-main-amosclaud-s-projects.vercel.app/ai-studio
+https://amosclaud-clean-git-main-amosclaud-s-projects.vercel.app/admin
