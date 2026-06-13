be59c88a06c3bb5adad3ef6e9432744e5e979e27
George makulu - Amosclaud <georgekbbito@gmail.com>
2026-06-13 00:36:51 -0500

    Delete src/app/marketplace/Page.tsx

--- src/app/marketplace/Page.tsx
+++
@@ -1,8 +0,0 @@
-export default function MarketplacePage() {
-  return (
-    <main style={{ padding: "30px" }}>
-      <h1>Marketplace</h1>
-      <p>Browse tools, services, prompts, and creator products.</p>
-    </main>
-  );
-}
